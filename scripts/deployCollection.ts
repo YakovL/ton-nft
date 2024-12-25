ec4a292aec8f17605cd30478dfc78607ebf9dfe2
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-12-25 13:18:03 +0300

    feature: deploy script

---
+++ scripts/deployCollection.ts
@@ -0,0 +1,38 @@
+import { toNano, Address } from '@ton/core';
+import { compile, NetworkProvider } from '@ton/blueprint';
+import { NFTCollection, nftContentToCell, NftCollectionConfig } from '../wrappers/NFTCollection';
+
+/**
+ * TODO: specify details here:
+ * - store collection content somewhere, specify collectionContentUri (choose a storage to your liking, IPFS is supported)
+ *   example: https://github.com/YakovL/ton-example-nft/raw/refs/heads/master/collection-1.json
+ *   specs: https://docs.ton.org/v3/guidelines/dapps/tutorials/nft-minting-guide#nft-specifications
+ * - collectionOwnerAddress: address from which other operations (like minting) will be executed
+ * - royaltyPercent: number, 2 float digits allowed (0.01 → 0.01%, 5 → 5%)
+ */
+const overallConfig = {
+    collectionContentUri: '',
+    collectionOwnerAddress: '',
+    royaltyPercent: 0,
+}
+
+export async function run(provider: NetworkProvider) {
+    const ownerAddress = Address.parse(overallConfig.collectionOwnerAddress);
+    const collectionConfig: NftCollectionConfig = {
+        ownerAddress: ownerAddress,
+        nextItemIndex: 0n,
+        nftItemCode: await compile('NFTItem'),
+        collectionContent: nftContentToCell({ uri: overallConfig.collectionContentUri }),
+        royaltyParams: NFTCollection.royaltyParamsToCell(overallConfig.royaltyPercent, ownerAddress)
+    };
+
+    const collection = provider.open(NFTCollection.createFromConfig(
+        collectionConfig,
+        await compile('NFTCollection')
+    ));
+
+    const estimatedDeployFee = toNano('0.0002'); // failed for 0.0001
+    await collection.sendDeploy(provider.sender(), estimatedDeployFee);
+
+    await provider.waitForDeploy(collection.address);
+}
